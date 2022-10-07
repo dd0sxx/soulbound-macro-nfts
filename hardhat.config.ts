db343d1e53694dd4d067ad43b3144075b14751bd
dd0sxx <contacttheotelonis@gmail.com>
2022-10-07 11:30:30 -0700

    cranked dat optimizooor

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,13 +11,24 @@ const config: HardhatUserConfig = {
       viaIR: false,
       optimizer: {
         enabled: true,
-        runs: 1000,
+        runs: 100000,
       },
     },
   },
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_GOERLI_PRIVATE_KEY}`,
+      accounts: [`${process.env.GOERLI_PRIVATE_KEY}`],
+    },
+  },
   gasReporter: {
-    enabled: (process.env.REPORT_GAS) ? true : false
-  }
-};
+    enabled: (process.env.REPORT_GAS) ? true : false,
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+}
 
 export default config;
